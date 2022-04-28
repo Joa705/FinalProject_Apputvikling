824eace7a25fd0e302ce878823689f296f7e5320
Joa705 <jpalbertse@hotmail.com>
2022-04-28 14:26:17 +0200

    Added authentication

--- App.js
+++ App.js
@@ -7,9 +7,10 @@ import Main from './pages/Main';
 import Room from './pages/Room';
 import EditPatient from './pages/EditPatient';
 import AddPatient from './pages/AddPatient';
-
+import Login from './pages/Login'
 
 LogBox.ignoreLogs(['Setting a timer'])
+LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and will be removed in a future release'])
 
 //Stacknavigator
 const Stack = createNativeStackNavigator();
@@ -20,6 +21,7 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login}/>
       <Stack.Screen name="Main" component={Main}/>
       <Stack.Screen name="EditPatient" component={EditPatient} />
       <Stack.Screen name="AddPatient" component={AddPatient} />
